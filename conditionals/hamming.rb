5ddf80e38623f972ba607c9626e2e730b165d86a
İhsan Mirage <tezravasen@gmail.com>
2024-11-25 16:45:11 +0300

    fix: change class name to Hamming and rename class method to compute due to tests

--- conditionals/hamming.rb
+++ conditionals/hamming.rb
@@ -1,12 +1,14 @@
-class HDistance
-  def self.calculate(word1, word2)
+class Hamming
+  def self.compute(word1, word2)
     raise ArguementError if word1.length != word2.length
+    distance = 0
+    word1.chars.each_with_index do |char, index|
+      distance += 1 if char != word2[index]
+    end
+    distance
   end
-  distance = 0
-  word1.chars.each_with_index do |char, index|
-    distance += 1 if char != word2[index]
-  end
-  distance
 end
+
+## bir stringi .chars methodu ile karakterlerine ayırabiliriz.
 
 ## bir stringi .chars methodu ile karakterlerine ayırabiliriz.
