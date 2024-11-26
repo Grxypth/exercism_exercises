3f4d8900afc15edb3a67f7f5ba34e8b8b75736e5
İhsan Mirage <tezravasen@gmail.com>
2024-11-26 14:40:07 +0300

    feat: complete exercise

--- ranges/ranges.rb
+++ ranges/ranges.rb
@@ -23,11 +23,21 @@ module Chess
   end
 
   def self.nick_name(first_name, last_name)
-    raise "Please implement the Chess.nick_name method"
+    nick = (first_name.slice(0, 2) + last_name.slice(-2, 2)).upcase
   end
 
   def self.move_message(first_name, last_name, square)
-    raise "Please implement the Chess.move_message method"
+    message = nick_name(first_name, last_name)
+    file = square[0]
+    rank = square[1].to_i
+    wholemessage =
+      (
+        if valid_square?(rank, file)
+          "#{message} moved to #{square}"
+        else
+          "#{message} attempted to move to #{square}, but that is not a valid square"
+        end
+      )
   end
 end
 
@@ -35,3 +45,6 @@ end
 ## constantlar çağırılırkende büyük harf ile yazılmalarına gerek vardır(bunu unutmam hata yapmama sebep oldu)
 ## ternary operatör kullanmam gereken yerde if else kullanmam gerektiğini fark ettim çünkü ternary operatörü "else" durumunuda ele alıyor buda is_valid_file/is_valid_rank sonucunu true bulmama rağmen loop tekrar döndüğünde değerin üzerine yazılmasına sebep oldu
 ## dolayısıyla burda tekil bir if ifadesini bir break ifadesi ile kullanmanın daha doğru olacağı sonucuna vardım.
+## kodumu en son chatgptye doğrulattığımda loop kullanmama gerek olmadığını, burda .include?() methodunu kullanarak problemi çok daha basit bir şekilde çözebileceğimi bana söyledi, katılıyorum fakat benim amacım loop kullanarak çözüm üretmek olduğu için bu şekilde bıraktım.
+## bir dahaki sefere böyle bir problem için include?() methodunu kullanacağım.
+##slice methodu ile stringin belirli bir kısmını alabiliriz
