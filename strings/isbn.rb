97015ecc1c088521e94748fc0415aa2671c2e4a9
İhsan Mirage <tezravasen@gmail.com>
2025-01-03 14:28:31 +0300

    isbn

---
+++ strings/isbn.rb
@@ -0,0 +1,21 @@
+class IsbnVerifier
+  def self.valid?(string)
+    nums = string.gsub("-", "")
+
+    return false if nums.length != 10
+
+    sum = 0
+
+    nums.chars.each_with_index do |char, index|
+      if index == 9 && char == "X"
+        sum += 10
+      elsif char >= "0" && char <= "9"
+        sum += char.to_i * (10 - index)
+      else
+        return false
+      end
+    end
+
+    return sum % 11 == 0
+  end
+end
