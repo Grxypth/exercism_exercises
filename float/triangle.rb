729603680c3e05c057c1db1d61efa110f018e5ba
İhsan Mirage <tezravasen@gmail.com>
2025-01-06 14:08:03 +0300

    start of triangle exercise

---
+++ float/triangle.rb
@@ -0,0 +1,22 @@
+class Triangle
+def initialize(a,b,c)
+@a=a
+@b=b
+@c=c
+end 
+ 
+ 
+  def equilateral?(a,b,c)
+
+if a==b && b==c
+  return true
+end
+
+  end
+  def isosceles?(a,b,c)
+  end
+  def scalene(a,b,c)
+  end
+
+
+end
