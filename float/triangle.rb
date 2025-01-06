38bbdb1938abc551bae1b8cc6f53e7fc86d52757
İhsan Mirage <tezravasen@gmail.com>
2025-01-06 14:25:08 +0300

    input dizi şeklinde veriliyor tek tek numara şeklinde değil düzeltme yaptım

--- float/triangle.rb
+++ float/triangle.rb
@@ -1,22 +1,33 @@
 class Triangle
-def initialize(a,b,c)
-@a=a
-@b=b
-@c=c
-end 
- 
- 
-  def equilateral?(a,b,c)
-
-if a==b && b==c
-  return true
-end
-
+  def initialize(sides)
+  @sides=sides
+  end 
+   
+   
+    def equilateral?
+     hash=triangularize
+      if hash[:a]==hash[:b] && hash[:b]==hash[:c]
+        return true
+      else
+        return false
   end
-  def isosceles?(a,b,c)
+  
+    end
+    def isosceles?(a,b,c)
+    end
+    def scalene(a,b,c)
+    end
+    
+    def triangularize
+      keys = [:a, :b, :c]
+      hash = keys.map.with_index { |key, index| [key, @sides[index]] }.to_h  
+      if hash[:a] + hash[:b] <= hash[:c] || hash[:a] + hash[:c] <= hash[:b] || hash[:b] + hash[:c] <= hash[:a] || hash[:a] <= 0 || hash[:b] <= 0 || hash[:c] <= 0
+        return false
+      else 
+        return hash
+      end
+    end
+  
+  
   end
-  def scalene(a,b,c)
-  end
-
-.
-end
+  
