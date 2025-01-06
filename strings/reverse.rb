56ccf7bb98ebed84949a2642ae7acfbea5a25aeb
İhsan Mirage <tezravasen@gmail.com>
2025-01-06 13:47:36 +0300

    reverse

---
+++ strings/reverse.rb
@@ -0,0 +1,6 @@
+class Reverser
+  def self.reverse(vb)
+    vb.reverse
+  end
+end
+## bu alıştırmada verilen bir stringi tersine çevirmemiz isteniyor bizde ruby standart methodlarından olan reverse methodunu kullanarak bu işlemi gerçekleştiriyoruz.
