06b27e7a61deacfb27d5d66814a633c50164a4d8
İhsan Mirage <tezravasen@gmail.com>
2025-01-06 13:44:20 +0300

    method adının on_earth olması gerekiyordu

--- float/spage.rb
+++ float/spage.rb
@@ -3,36 +3,36 @@ class SpaceAge
     @second = second
   end
 
-  def earth_time
+  def on_earth
     @second / (60 * 60 * 24 * 365.25)
   end
 
   def on_mercury
-    earth_time / 0.2408467
+    on_earth / 0.2408467
   end
 
   def on_venus
-    earth_time / 0.61519726
+    on_earth / 0.61519726
   end
 
   def on_mars
-    earth_time / 1.8808158
+    on_earth / 1.8808158
   end
 
   def on_jupiter
-    earth_time / 11.862615
+    on_earth / 11.862615
   end
 
   def on_saturn
-    earth_time / 29.447498
+    on_earth / 29.447498
   end
 
   def on_uranus
-    earth_time / 84.016846
+    on_earth / 84.016846
   end
 
   def on_neptune
-    earth_time / 164.79132
+    on_earth / 164.79132
   end
 end
 
