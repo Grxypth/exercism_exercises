b36b4be4b9f0b258455789d332ebf88ca017d752
İhsan Mirage <tezravasen@gmail.com>
2025-01-06 16:44:52 +0300

    alıştırmayı bitir diffrence methodu

--- numbers/sqn.rb
+++ numbers/sqn.rb
@@ -1,23 +1,29 @@
 ## Bu egzerzisde karelerin toplamı ile toplamların karesi arasındaki farkı hesaplayacağız.
 ##karelerin toplamı için  [n(n+1)(2n+1)]/6 formülünü kullanacağım.
-##toplamların karesi için ise n^2(n+1)^2/4 formülünü kullanacağım
+
 
 class Squares
 
-def initialize(number)
-  @number=number
-end
-
-
-
-
-def sum_of_squares
-@number*(@number+1)*(2*@number+1)/6
-end
-
-
-def square_of_sum
-@number*@number*(@number+1)*(@number+1)/4
-end
-
-end
+  def initialize(number)
+    @number=number
+  end
+  
+  
+  
+  
+  def sum_of_squares
+  @number*(@number+1)*(2*@number+1)/6
+  end
+  
+  
+  def square_of_sum
+  @number*@number*(@number+1)*(@number+1)/4
+  end
+  
+  def difference
+  result=square_of_sum-sum_of_squares
+  end
+  
+    
+  
+  end
