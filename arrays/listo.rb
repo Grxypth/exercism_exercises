eb71b3116838d73a0f2526c81020e7a6ba79d5de
İhsan Mirage <tezravasen@gmail.com>
2025-01-09 09:18:29 +0300

    list ops project

---
+++ arrays/listo.rb
@@ -0,0 +1,19 @@
+class ListOps
+  def arrays
+  end
+
+  def reverser(arr)
+  end
+
+  def concantter(input1, input2)
+  end
+
+  def mapper()
+  end
+
+  def sum_reducer
+  end
+
+  def factorial_reducer(input)
+  end
+end
