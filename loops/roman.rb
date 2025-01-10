b37b626af3bfe4324994fce9426d031c9f8e859c
İhsan Mirage <tezravasen@gmail.com>
2025-01-10 11:51:04 +0300

    hata:Line 4: syntax error, unexpected constant, expecting '}'

--- loops/roman.rb
+++ loops/roman.rb
@@ -1,3 +1,22 @@
 def to_roman(num)
+  numerals={
+  M: 1000
+  D: 500
+  C: 100
+  L: 50
+  X: 10 
+  V: 5
+  I: 1
+  }
+  roman=''
+  numerals.each do |key,value|
+    while num>=value
+      result+=roman.to_s
+      num-=value
+  end
 
 end
+
+
+## Bu alıştırmada Arap rakamlarıyla verilen rakamları roma rakamlarına çevirip döndürmemiz isteniyor.
+## Bunu ben verilen sayıyı basamaklarına bölüp her basamağına karşılık gelen roma rakamını bulup birleştirerek yapmayı planlıyorum.
