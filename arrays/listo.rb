19079f2f9faa33cee3eb73874eb9d2a71917d2ed
İhsan Mirage <tezravasen@gmail.com>
2025-01-10 11:12:33 +0300

    feat: finish exercise

--- arrays/listo.rb
+++ arrays/listo.rb
@@ -1,26 +1,33 @@
 class ListOps
   def self.arrays(arr)
-    count = 0
-    arr.each { |num| count += 1 }
+    count=0
+    arr.each do |num| 
+      count += 1
+    end
     count
   end
 
   def self.reverser(arr)
-    arr.reverse
+arr.reverse
+end
+
+  def self.concatter(input1, input2)
+    input1.concat(input2)
   end
 
-  def self.concantter(input1, input2)
+  def self.mapper(fnc)
+        fnc.map{ |blk| yield(blk) }
   end
 
-  def self.mapper()
+  def self.sum_reducer(input)
+            input.reduce(0){ |num1, num2| num1 + num2 }
   end
 
-  def self.sum_reducer
-  end
-
-  def self.filterer(arr)
+  def self.filterer(input)
+            input.select{ |blk| yield(blk) }
   end
 
   def self.factorial_reducer(input)
+        input.reduce(1){ |num1, num2| num1 * num2 }
   end
 end
